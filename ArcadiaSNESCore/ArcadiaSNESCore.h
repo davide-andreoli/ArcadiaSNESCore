a940168133fdcaa11fbd94f037bf170f7bd4ff36
Davide Andreoli <davide.andreoli@icloud.com>
2024-06-27 08:27:12 +0200

    Initial Commit

---
+++ ArcadiaSNESCore/ArcadiaSNESCore.h
@@ -0,0 +1,18 @@
+//
+//  ArcadiaSNESCore.h
+//  ArcadiaSNESCore
+//
+//  Created by Davide Andreoli on 27/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArcadiaSNESCore.
+FOUNDATION_EXPORT double ArcadiaSNESCoreVersionNumber;
+
+//! Project version string for ArcadiaSNESCore.
+FOUNDATION_EXPORT const unsigned char ArcadiaSNESCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArcadiaSNESCore/PublicHeader.h>
+
+
